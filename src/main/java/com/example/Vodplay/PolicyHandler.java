a91afc44628e0f1d4b380b0d04c20b9b33854074
net3754 <63759252+net3754@users.noreply.github.com>
2020-07-03 14:18:47 +0900

    Update PolicyHandler.java

--- src/main/java/com/example/Vodplay/PolicyHandler.java
+++ src/main/java/com/example/Vodplay/PolicyHandler.java
@@ -1,27 +1,9 @@
 package com.example.Vodplay;
 
-import java.util.Optional;
+import org.springframework.stereotype.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.cloud.stream.messaging.Processor;
+@Service
+public class PolicyHandler{
+    
 
-public class PolicyHandler {
-
-	@Autowired
-	VodPurchaseRepository vodPurchaseRepository;
-	
-	@StreamListener(Processor.INPUT)
-	public void onVodRegister(@Payload PaymentApproved paymentApproved) {
-		if( paymentApproved.getEventType().equals("PaymentApproved")){
-			System.out.println("=========PaymentApproved STR=============");
-			
-			Long id = paymentApproved.getPayId();       
-	        Optional<VodPurchase> vodById = vodPurchaseRepository.findById(id);
-	        VodPurchase p = vodById.get();
-			vodPurchaseRepository.save(p);
-			System.out.println("=========PaymentApproved END=============");
-		}
-	}
 }
